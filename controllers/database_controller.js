92a4495518de70fb30fa85bf99240bdea569ee69
ebietanmi <nachurboi@gmail.com>
2026-07-27 14:45:02 +0100

    first portfolio website commit

---
+++ controllers/database_controller.js
@@ -0,0 +1,276 @@
+import dotenv from 'dotenv'
+import mysql2 from 'mysql2';
+import bcrypt from 'bcrypt'
+import { response } from 'express';
+dotenv.config();
+
+
+// Creates database schema for adding projects on first use.
+async function createProjectSchema(pool) {
+    const query = `CREATE TABLE project_table (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    project_title VARCHAR(255) NOT NULL,
+    project_author VARCHAR(255) DEFAULT "No Author",
+    project_description TEXT,
+    project_date_created DATETIME DEFAULT NOW(),
+    project_img_url VARCHAR(255),
+    project_public_id VARCHAR(255)
+)`
+    pool.query(query)
+}
+// Creates database schema for adding blogs on first use.
+async function createBlogSchema(pool) {
+    const query = `CREATE TABLE blog_table (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    blog_title VARCHAR(255) NOT NULL,
+    blog_excerpt VARCHAR(255) DEFAULT "No excerpt",
+    blog_content TEXT,
+    blog_creation_date DATETIME DEFAULT NOW(),
+    blog_category VARCHAR(255) NOT NULL,
+    blog_img_url VARCHAR(255),
+    blog_public_id VARCHAR(255)
+)`
+    pool.query(query)
+}
+// Creates database schema for adding admin on first use.
+async function createAdminSchema(pool) {
+    const query = `CREATE TABLE admin_table (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role    VARCHAR(255) NOT NULL DEFAULT "user"
+)`
+    pool.query(query)
+}
+// Creates database schema for recieved mail.
+async function createRecievedMailSchema(pool) {
+    const query = `CREATE TABLE recieved_email_table (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    sender_name VARCHAR(255) NOT NULL,
+    sender_email VARCHAR(255) NOT NULL,
+    subject VARCHAR(255) DEFAULT 'Message from portfolio website',
+    message    VARCHAR(255) NOT NULL
+)`
+    pool.query(query)
+}
+
+
+
+// Creates admin profile on first use.
+async function createPrimaryAdmin(pool) {
+    const hashedPassword = await bcrypt.hash('admin', 10)
+    createUserSQL('admin', hashedPassword, 'admin')
+}
+
+
+//Checks if database exists? does nothing of it exists and create a database if it deoes not!
+async function ensureDatabaseExists() {
+    const database = process.env.MYSQL_DATABASE;
+    const connenction = await mysql2.createConnection({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+    }).promise()
+
+    const [rows] = await connenction.execute(`SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = ?`, [database]);
+    if (rows.length === 0) {
+        await connenction.execute(`CREATE DATABASE\`${database}\``)
+        const pool = mysql2.createPool({
+            host: process.env.MYSQL_HOST,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD,
+            database: process.env.MYSQL_DATABASE,
+            waitForConnections: true,
+            connectionLimit: 10
+        }).promise()
+        await createAdminSchema(pool);
+        await createProjectSchema(pool);
+        await createPrimaryAdmin(pool);
+        await createRecievedMailSchema(pool);
+        await createBlogSchema(pool);
+    } else {
+        null;
+    }
+    await connenction.end();
+}
+ensureDatabaseExists()
+
+
+const pool = mysql2.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10
+}).promise();
+
+// const pool = mysql2.createPool({
+//     uri: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false }
+// }
+// ).promise();
+
+
+
+export async function createUserSQL(username, password, role) {
+    const query = `INSERT INTO admin_table (username, password, role) VALUES (?, ?, ?)`;
+    try {
+        const [response] = await pool.query(query, [username, password, role]);
+        if (response.affectedRows > 0) {
+            return { 'ok': true, "SQLMessage": 'User created succesfully', 'insertId': response.insertId }
+        }
+        else {
+            return { 'ok': false, "SQLMessage": 'Failed to create user' }
+        }
+    } catch (error) {
+        if (error instanceof Error) {
+            if (error.errno === 1062) {
+                return { 'ok': false, 'taken': true, "SQLMessage": 'Username is already taken' }
+            } else {
+                return { 'ok': false, "SQLMessage": error }
+            }
+        }
+    }
+}
+
+export async function getUsersSQL() {
+    const query = 'SELECT * FROM admin_table';
+    const [users] = await pool.query(query);
+    if (users.length > 0) { return users } else { return { "SQLMessage": 'No user found' } }
+}
+
+export async function getUserSQL(id) {
+    const query = `SELECT * FROM admin_table WHERE id = ?`;
+    const [user] = await pool.query(query, [id])
+    if (user.length > 0) { return user } else { return { "SQLMessage": 'No user found' } }
+}
+
+export async function updatePasswordSQL(id, password) {
+    const query = 'UPDATE admin_table SET password = ? WHERE id = ?'
+    const [response] = await pool.query(query, [password, id]);
+    if (response.affectedRows > 0) {
+        return { "SQLMessage": 'Password Update Sucessful', 'ok': true }
+
+    } else {
+        return { "SQLMessage": 'Password Update Failed', 'ok': false }
+    }
+}
+
+export async function checkUserSQL(username) {
+    const query = "SELECT id, password FROM admin_table WHERE username = ?";
+    const [rows] = await pool.query(query, [username]);
+    if (rows.length > 0) { return rows[0] }
+    else return { "SQLMessage": 'User not found' };
+}
+
+export async function createProjectSQL(project_title, project_author,
+    project_description, project_img_url, project_public_id) {
+    const query = `INSERT INTO project_table ( 
+    project_title,
+    project_author,
+    project_description, 
+    project_img_url,
+    project_public_id
+    ) 
+    VALUES(?,?,?,?,?)`;
+    const [response] = await pool.query(
+        query, [project_title, project_author, project_description, project_img_url, project_public_id]
+    );
+    return response;
+}
+
+
+export async function createBlogSQL({ blog_title, blog_excerpt, blog_content,
+    blog_creation_date, blog_category, blog_img_url, blog_public_id }) {
+    const query = `INSERT INTO blog_table ( 
+    blog_title,
+    blog_excerpt,
+    blog_content,
+    blog_creation_date,
+    blog_category,
+    blog_img_url,
+    blog_public_id
+    ) 
+    VALUES(?,?,?,?,?,?,?)`;
+    const [response] = await pool.query(
+        query, [blog_title, blog_excerpt, blog_content, blog_creation_date, blog_category, blog_img_url, blog_public_id]
+    );
+    return response;
+}
+
+export async function getBlogsSQL() {
+    const query = `SELECT * FROM blog_table`;
+    try {
+        const [blogs] = await pool.query(query);
+        if (blogs.length > 0) {
+            return blogs;
+        } else return { "SQLMessage": 'No Blog found' };
+    }
+    catch (error) {
+        return error;
+    }
+
+}
+
+export async function getProjectSQL(id) {
+    const query = 'SELECT * FROM project_table WHERE id = ?';
+    try {
+        const [project] = await pool.query(query, [id]);
+        if (project.length > 0) {
+            return project
+        } else {
+            return { "SQLMessage": `No Matching Project` }
+        }
+    } catch (error) {
+        return { "SQLMessage": `Unable to retrieve project with error : ${error}` }
+    }
+}
+
+export async function getProjectsSQL() {
+    const query = `SELECT * FROM project_table`;
+    try {
+        const [projects] = await pool.query(query);
+        if (projects.length > 0) {
+            return projects;
+        } else return { "SQLMessage": 'No projects found' };
+    }
+    catch (error) {
+        return error;
+    }
+
+}
+export async function deleteProjectSQL(id) {
+    const query = `DELETE FROM project_table WHERE id = ?`;
+    try {
+        const [response] = await pool.query(query, [id]);
+        if (response.affectedRows > 0) {
+            return { "SQLMessage": 'Project Deleted', "ok": true }
+        }
+        else {
+            return { "SQLMessage": 'Project not Deleted', "ok": false }
+        }
+    } catch (error) {
+        return { "SQLMessage": error, "ok": false };
+    }
+}
+
+export async function createRecievedMailSQL(sender_name, sender_email, subject, message) {
+    const query = `INSERT INTO recieved_email_table ( 
+    sender_name,
+    sender_email,
+    subject,
+    message
+    ) 
+    VALUES(?,?,?,?)`;
+    const [result] = await pool.query(
+        query, [sender_name, sender_email, subject, message]
+    );
+    if (result.affectedRows > 0) {
+        return { "SQLMessage": 'Message sent saved', "ok": true }
+    }
+    else {
+        return { "SQLMessage": 'Message not saved', "ok": false }
+    }
+
+    return result;
+}
---
+++ server.js
@@ -0,0 +1,393 @@
+import express, { json, response } from 'express';
+import cors from 'cors';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import multer from 'multer';
+import path from 'path';
+import nodemailer from 'nodemailer';
+import { v2 as cloudinary } from 'cloudinary';
+import {
+    createUserSQL, getUserSQL,
+    getUsersSQL, checkUserSQL, createProjectSQL, createBlogSQL,
+    getProjectsSQL, getProjectSQL,
+    deleteProjectSQL, updatePasswordSQL, createRecievedMailSQL,
+    getBlogsSQL
+} from './controllers/database_controller.js';
+import { errorMonitor } from 'events';
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+
+});
+
+const app = express();
+app.use(cors()) // 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+
+
+// Handles and report errors tha may arise while starting the server.
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ 'message': 'Something broke!' });
+})
+
+//This API route handles te creation of new user.
+export async function createUser() {
+    const authMiddleware = async (req, res, next) => {
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ error: 'No token' })
+        }
+        else {
+            try {
+                const decoded = await jwt.verify(token, process.env.JWT_SECRET)
+                req.user = { id: decoded.userId }
+                next()
+            } catch (error) { return res.status(401).json({ error: "Invalid token" }) }
+        }
+    }
+    const user = await app.post('/create-user', authMiddleware, async (req, res) => {
+        const { username, password, role } = req.body;
+        try {
+            const hashedPassword = await bcrypt.hash(password, 10);
+            const response = await createUserSQL(username, hashedPassword, role);
+            if (response.ok) {
+                res.status(201).json({ 'ok': true, 'status': 201, 'message': response.SQLMessage, userId: response.insertId });
+            }
+            else if (response.taken) {
+                res.status(401).json({ 'ok': false, 'status': 401, 'message': response.SQLMessage });
+            }
+            else {
+                res.status(500).json({ 'ok': false, 'status': 500, 'message': response.SQLMessage })
+            }
+
+        }
+        catch (error) {
+            if (error instanceof Error)
+                res.status(500).json({ 'ok': false, 'status': 500, 'message': 'An error occurred while creating user' });
+        }
+    }
+    );
+}
+
+//This API route handling creation of new user.
+export async function fetchUsers() {
+    const users = await app.get('/users', async (req, res) => {
+        try {
+            const data = await getUsersSQL();
+            res.status(200).send(data);
+        } catch (error) {
+            console.error('Error fetching users:', error);
+            res.status(500).send({ error: 'An error occurred while fetching users' });
+        }
+    })
+}
+
+//This API route handles the finding of one user.
+export async function findOneUser(id) {
+    const user = await app.get('/users/:id', async (req, res) => {
+        const id = req.params.id;
+        try {
+            const data = await getUserSQL(id);
+            res.status(200).send(data);
+        } catch (error) {
+            res.status(500).send({ error: 'An error occurred while fetching user' });
+        }
+    })
+}
+
+//This API route handles the checking if user exists.
+export async function logIn() {
+    const data = await app.post('/login', async (req, res) => {
+        const { username, password } = req.body;
+        try {
+            const user = await checkUserSQL(username);
+            if (user === null) {
+                res.status(401).json({ 'message': "Invalid Username or Password" })
+            } else {
+                const isMatch = await bcrypt.compare(password, user.password);
+                if (isMatch) {
+                    const token = jwt.sign({ userId: user.id, username: username },
+                        process.env.JWT_SECRET,
+                        { expiresIn: '15m' }
+                    );
+                    res.status(201).json({ token, 'ok': true, id: user.id, user: username });
+                }
+                else { res.status(401).json({ 'ok': false }) }
+            }
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    })
+}
+
+//This API route handles the creation of new user.
+async function createProject() {
+    // This sets up multer to facilitate uploading of file.
+    const storage = multer.memoryStorage();
+    const upload = multer({
+        storage,
+        limits: { fileSize: 5 * 1024 * 1024 }
+    });
+
+    await app.post('/create-project', upload.single('img_file'), async (req, res) => {
+        const { project_title, project_author, project_description } = req.body;
+        try {
+            const result = await new Promise((resolve, reject) => {
+                cloudinary.uploader.upload_stream(
+                    { folder: 'portfolio/projects' }, // path for desired cloudinary folder
+                    (error, result) => {
+                        if (error) reject(error)
+                        else resolve(result)
+                    }
+                ).end(req.file.buffer)
+            })
+            const project_img_url = result.secure_url;
+            const project_public_id = result.public_id;
+            const sql_response = await createProjectSQL(
+                project_title,
+                project_author,
+                project_description,
+                project_img_url,
+                project_public_id
+            )
+            if (sql_response.affectedRows > 0) {
+                res.status(200).json({ 'status': 200, 'ok': true, 'message': 'project created succesfully', });
+            } else {
+                res.status(501).json({ 'status': 502, 'ok': false, 'message': 'unable to create project', })
+            }
+        } catch (error) {
+            if (error.errno === -3008) {
+                res.status(502).json({ 'ok': false, 'status': 501, 'message': 'Could not connect, please check your internet' });
+            } else {
+                res.status(500).json({ 'ok': false, 'status': 500, 'message': 'Internal Server error, try later' });
+            }
+        }
+
+    })
+
+}
+
+// This API gets a single project using the ID
+export async function getProject(id) {
+    const response = app.get('/projects/:id', async (req, res) => {
+        const id = req.params.id;
+        const data = await getProjectSQL(id)
+        data.length > 0 ? res.status(201).send(data) : res.status(401).send({ "Message": 'No project Found' })
+    });
+
+}
+
+//This API route handles fetching odf all project.
+export async function getProjects() {
+    const response = app.get("/projects", async (req, res) => {
+        try {
+            const data = await getProjectsSQL();
+            data.length > 0 ? res.status(200).send(data) : res.status(401).send({ "message": data.SQLMessage })
+        } catch (error) {
+            console.error('Error fetching projects:', error);
+            res.status(500).send({ error: 'An error occurred while fetching projects' });
+        }
+    })
+}
+
+export async function deleteProject() {
+    try {
+        await app.delete('/delete-project/:id', async (req, res) => {
+            const id = req.params.id;
+            const response = await deleteProjectSQL(id);
+            response.ok ? res.status(201).send(response) : res.status(401).send(response)
+        })
+    } catch (error) {
+        throw new Error(error);
+    }
+
+}
+//This API route handles the creation of new blog.
+async function CreateBlog() {
+    //Multer set up for blog
+    const storage = multer.memoryStorage();
+    const upload = multer({
+        storage,
+        limits: { fileSize: 5 * 1024 * 1024 }
+    });
+    await app.post('/create-blog', upload.single('blog_file'), async (req, res) => {
+        const { blog_title, blog_excerpt, blog_content, blog_creation_date, blog_category } = req.body;
+        try {
+            const result = await new Promise((resolve, reject) => {
+                cloudinary.uploader.upload_stream(
+                    { folder: 'portfolio/blogs' }, // optional folder in Cloudinary
+                    (error, result) => {
+                        if (error) reject(error)
+                        else resolve(result)
+                    }
+                ).end(req.file.buffer)
+            })
+            const blog_img_url = result.secure_url;
+            const blog_public_id = result.public_id;
+
+            const sql_response = await createBlogSQL(
+                {
+                    blog_title, blog_excerpt, blog_content, blog_creation_date,
+                    blog_category, blog_img_url, blog_public_id
+                }
+            );
+            if (sql_response.affectedRows > 0) {
+                res.status(200).json({ 'status': 200, 'ok': true, 'message': response.SQLMessage });
+            } else {
+                res.status(500).json({ 'status': 500, 'ok': false, 'message': response.SQLMessage })
+            }
+
+        } catch (error) {
+            console.error(error);
+            if (error.errno === -3008) {
+                res.status(502).json({ 'status': 502, 'ok': false, 'message': 'Could not connect, please check your internet' });
+            } else {
+                res.status(500).json({ 'status': 500, 'ok': false, 'message': 'Internal Server error, try later' });
+            }
+        }
+
+    })
+
+}
+
+export async function getBlogs() {
+    const response = app.get("/blog", async (req, res) => {
+        try {
+            const data = await getBlogsSQL();
+            data.length > 0 ? res.status(200).send(data) : res.status(401).send({ "message": data.SQLMessage })
+        } catch (error) {
+            console.error('Error fetching projects:', error);
+            res.status(500).send({ error: 'An error occurred while fetching blogs' });
+        }
+    })
+}
+
+
+export async function resetPassword() {
+    const authMiddleware = async (req, res, next) => {
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ error: 'No token' })
+        }
+        else {
+            try {
+                const decoded = await jwt.verify(token, process.env.JWT_SECRET)
+                req.user = { id: decoded.userId }
+                next()
+            } catch (error) { return res.status(401).json({ error: "Invalid token" }) }
+        }
+    }
+    const response = await app.put('/reset-password', authMiddleware, async (req, res) => {
+        try {
+            const userId = req.user.id;
+            const { oldPassword, newPassword } = req.body;
+            if (!oldPassword || !newPassword) return res.status(400).json({ 'message': 'All field required', "ok": false })
+            const [user] = await getUserSQL(userId);
+            const isMatch = await bcrypt.compare(oldPassword, user.password);
+            if (!isMatch) {
+                res.status(404).json({ "message": 'Old Password Incorrect', "ok": false })
+            } else {
+                const hashedPassword = await bcrypt.hash(newPassword, 10)
+                await updatePasswordSQL(userId, hashedPassword).
+                    then(res.status(200).json({ 'ok': true, 'message': 'Password updated successfully' }));
+            }
+        } catch (error) {
+            res.status(500).json({ error: 'Sever Error', 'message': error })
+        }
+    },
+    )
+
+}
+
+// EMAIL SETUP
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.USER_EMAIL,
+        pass: process.env.USER_EMAIL_PASSWORD
+    }
+});
+
+// CHECK FOR CONNECTIVITY
+async function checkMailNetworkStatus() {
+    await app.get('/check-mail-network', async (req, res) => {
+        try {
+            const response = await transporter.verify(
+                (error, success) => {
+                    if (error) {
+                        res.status(501).json({ 'status': 500, 'ok': false, "message": 'Network is unstable' })
+                    } else {
+                        res.status(201).json({ 'status': 201, 'ok': true, "message": 'Network is stable send mail' })
+                    }
+                }
+            );
+        } catch (error) {
+            res.status(500)
+                .json({ 'status': 500, 'ok': false, "message": 'Network unstable or server error, try again later' })
+        }
+    })
+}
+
+export async function sendAndSaveMail() {
+    await app.post("/send-mail", async (req, res) => {
+        const { name, email, subject, message } = req.body;
+        const mailOptions = {
+            name: name,
+            email: email, subject: subject,
+            text: message,
+            to: process.env.USER_EMAIL
+        }
+        try {
+            await transporter.sendMail(mailOptions, async (error, info) => {
+                if (error) {
+                    console.error('Error sending email:', error);
+                    res.status(500).json({ 'ok': false, 'message': 'Unable to send email' });
+                }
+                else {
+                    const sql_response = await createRecievedMailSQL(mailOptions.name, mailOptions.email, mailOptions.subject, mailOptions.text);
+                    if (sql_response.ok) {
+                        res.status(200).json({ 'ok': true, 'message': 'Email sent successfully' })
+                    }
+                    else {
+                        res.status(500).json({ 'ok': false, 'message': 'Email sent but not saved' })
+                    }
+                }
+            }
+            );
+        } catch (error) {
+            res.status(500).json({ 'ok': false, 'message': 'Internal Server error, Email not sent' })
+        }
+    }
+    );
+}
+
+
+// Starting the server.
+app.listen(process.env.PORT, () => {
+    console.log(`Sever is up and running on port ${process.env.PORT}`)
+});
+
+
+
+createUser();
+fetchUsers();
+findOneUser();
+logIn();
+createProject();
+getProject()
+getProjects();
+deleteProject();
+resetPassword();
+sendAndSaveMail();
+CreateBlog();
+getBlogs();
+checkMailNetworkStatus();
+
