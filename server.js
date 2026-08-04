df3e7bdd02c1133c5d6c3aa169fa7e82a04c2e13
ebietanmi <nachurboi@gmail.com>
2026-08-04 20:41:05 +0100

    working on resend

--- server.js
+++ server.js
@@ -367,7 +367,7 @@ async function checkMailNetworkStatus() {
 
 export async function sendAndSaveMail() {
     await app.post("/send-mail", async (req, res) => {
-        // const { name, email, subject, message } = req.body;
+        const { name, email, subject, message } = req.body;
         // try {
         //     await resend.emails.send({
         //         from: `${name}, <${email}>`,
@@ -389,7 +389,7 @@ export async function sendAndSaveMail() {
         //     console.log("From Node",error)
         //     res.status(500).json({ 'ok': false, 'message': 'Internal Server error, Email not sent' })
         // }
-        res.status(200).json({ 'ok': true, 'message': 'Email sent successfully' });
+        res.status(200).json({ 'ok': true, 'message':{ name, email, subject, message }});
     }
     );
 }
